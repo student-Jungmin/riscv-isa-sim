af1c324636a6a1da717a383dbc694523afac50bc
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2025-08-14 13:14:24 +0000

    [PyTorchSim] Support Sin,cos instruction

--- disasm/disasm.cc
+++ disasm/disasm.cc
@@ -788,6 +788,8 @@ disassembler_t::disassembler_t(int xlen)
   DEFINE_RTYPE(torchsim_vexp);
   DEFINE_RTYPE(torchsim_verf);
   DEFINE_RTYPE(torchsim_vtanh);
+  DEFINE_RTYPE(torchsim_vsin);
+  DEFINE_RTYPE(torchsim_vcos);
   DEFINE_RTYPE(torchsim_vlane_idx);
   add_insn(new disasm_insn_t("snez", match_sltu, mask_sltu | mask_rs1, {&xrd, &xrs2}));
   DEFINE_RTYPE(sltu);
---
+++ riscv/insns/torchsim_vcos.h
@@ -0,0 +1,18 @@
+// torchsim_verf
+
+const reg_t vs = insn.rs2();
+const reg_t vd = insn.rd();
+const reg_t vl = P.VU.vl->read();
+const reg_t n_vu = P.VU.get_vu_num();
+const reg_t vstart = P.VU.vstart->read();
+
+for (reg_t vu_idx=0; vu_idx<n_vu; vu_idx++) {
+    P.VU.vstart->write(vstart);
+    for (reg_t i=0; i<vl; i++) {
+        VI_STRIP(i);
+        P.VU.vstart->write(i);
+        float val = P.VU.elt<float>(vs, vreg_inx, vu_idx, true);
+        P.VU.elt<float>(vd, vreg_inx, vu_idx, true) = cos(val);
+    }
+}
+P.VU.vstart->write(0);
