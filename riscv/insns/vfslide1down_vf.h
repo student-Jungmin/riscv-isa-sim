090995fa15a67a8e9093a70b181376a469179ac3
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2026-03-10 21:39:04 +0900

    [Fix] add missing vu_idx in vector instruction

--- riscv/insns/vfslide1down_vf.h
+++ riscv/insns/vfslide1down_vf.h
@@ -23,13 +23,13 @@ if (i != vl - 1) {
 } else {
   switch (P.VU.vsew) {
     case e16:
-      P.VU.elt<float16_t>(rd_num, vl - 1, true) = f16(FRS1);
+      P.VU.elt<float16_t>(rd_num, vl - 1, vu_idx, true) = f16(FRS1);
       break;
     case e32:
-      P.VU.elt<float32_t>(rd_num, vl - 1, true) = f32(FRS1);
+      P.VU.elt<float32_t>(rd_num, vl - 1, vu_idx, true) = f32(FRS1);
       break;
     case e64:
-      P.VU.elt<float64_t>(rd_num, vl - 1, true) = f64(FRS1);
+      P.VU.elt<float64_t>(rd_num, vl - 1, vu_idx, true) = f64(FRS1);
       break;
   }
 }
--- riscv/insns/vfslide1up_vf.h
+++ riscv/insns/vfslide1up_vf.h
@@ -23,13 +23,13 @@ if (i != 0) {
 } else {
   switch (P.VU.vsew) {
     case e16:
-      P.VU.elt<float16_t>(rd_num, 0, true) = f16(FRS1);
+      P.VU.elt<float16_t>(rd_num, 0, vu_idx, true) = f16(FRS1);
       break;
     case e32:
-      P.VU.elt<float32_t>(rd_num, 0, true) = f32(FRS1);
+      P.VU.elt<float32_t>(rd_num, 0, vu_idx, true) = f32(FRS1);
       break;
     case e64:
-      P.VU.elt<float64_t>(rd_num, 0, true) = f64(FRS1);
+      P.VU.elt<float64_t>(rd_num, 0, vu_idx, true) = f64(FRS1);
       break;
   }
 }
